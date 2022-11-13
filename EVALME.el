49b2a25407ff4e2169211344897a591f830eb50e
Germán Palomares Pizarro <chief_keef@riseup.net>
2022-11-13 14:35:57 +0100

    whoops

--- EVALME.el
+++ EVALME.el
@@ -1,17 +1,14 @@
 ;; Change this
-;; Change this
 (setq org-publish-project-alist
-     '(("qorg11.net"
-            :base-directory "/home/qorg/docs/repos/qorg_org"
-            :publishing-directory "/home/qorg/docs/repos/qorg_org/out_html"
+     '(("suragu.net"
+            :base-directory "~/repos/suragu_org"
+            :publishing-directory "~/repos/suragu_org/out_html"
             :section-numbers nil
             :publishing-function org-html-publish-to-html
             :table-of-contents nil
             :recursive t
             )))
 
-(defun make-qorg ()
+(defun make-suragu ()
      (interactive)
-     (org-publish "qorg11.net"))
-
-
+     (org-publish "suragu.net"))
